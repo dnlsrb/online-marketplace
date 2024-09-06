a621b3e50f3d562132881fbdbb17b92e61bd5d57
dnlsrb <144025551+dnlsrb@users.noreply.github.com>
2024-09-07 00:16:08 +0800

    Update nav.blade.php

--- resources/views/components/sections/nav.blade.php
+++ resources/views/components/sections/nav.blade.php
@@ -33,12 +33,12 @@
     <form action="" class=" flex  flex-1 w-32 relative my-5 mx-0   md:mx-20">
       @csrf
 
-      <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none w-full  ">
+      <div class="absolute inset-y-0 start-0   items-center ps-3 pointer-events-none w-full  md:flex hidden  ">
         <i class="fa-solid fa-magnifying-glass"></i>
       </div>
     
      
-        <input type="search" id="default-search" class="block w-full   p-4  ps-10 text-sm text-gray-900 border border-gray-300  rounded-sm bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Product Name" required />
+        <input type="search" id="default-search" class="block w-full   p-4  ps-2 md:ps-10 text-sm text-gray-900 border border-gray-300  rounded-sm bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Product Name" required />
         <input type="submit" name="submit" value="Search" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm  px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> 
      
       
