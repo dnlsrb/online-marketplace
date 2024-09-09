139352e8104aa22fe9f7d929a96e07ae37ba45ba
dnlsrb <144025551+dnlsrb@users.noreply.github.com>
2024-09-10 01:08:30 +0800

    Update index.blade.php

--- resources/views/pages/seller/index.blade.php
+++ resources/views/pages/seller/index.blade.php
@@ -2,7 +2,7 @@
 
     <button x-data="" x-on:click.prevent="$dispatch('open-modal', 'subcription-open')"  value="Buy Subcription" 
     class="flex  text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded"> 
-    Apply as Seller
+    Add Product
   </button>
         <x-vendor.breeze.modal name="subcription-open" >
             
