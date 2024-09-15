b45617b184415e2e9ceec66699bd708ffa64b4a9
dnlsrb <144025551+dnlsrb@users.noreply.github.com>
2024-09-15 15:22:00 +0800

    added remove

--- resources/views/components/customer/sidebar.blade.php
+++ resources/views/components/customer/sidebar.blade.php
@@ -3,7 +3,7 @@
     <x-customer.minisidebar/>
  
      
-<aside id="default-sidebar" class="sticky bg-white  border top-0 left-0 z-40 grow-0 w-96 h-screen mx-3  transition-transform -translate-x-full sm:translate-x-0 hidden md:block" aria-label="Sidebar">
+<aside id="default-sidebar"  x-transition class="sticky bg-white  border top-0 left-0 z-40 grow-0 w-96 h-screen mx-3  transition-transform -translate-x-full sm:translate-x-0 hidden md:block" aria-label="Sidebar">
   <div class="h-full px-3 py-4  ">
     <x-customer.menu/>
   </div>
--- resources/views/pages/customer/cart/index.blade.php
+++ resources/views/pages/customer/cart/index.blade.php
@@ -9,66 +9,106 @@
         <div class="mx-auto w-full flex-none  xl:max-w-7xl">
 
             <h1 class="text-2xl mb-5 font-semibold">Shopping Cart</h1>
-
-            <div class="overflow-x-auto bg-white p-1 rounded"  >
+          
+            <div class="overflow-x-auto bg-white p-1 rounded" x-data="{ count: 0 }" >
                 @forelse ($cart->cartProducts as $c_product)
                     <div class="flex   items-center my-2 border-0  border-b p-2 ">
                         <div class="flex flex-row   items-center  "> 
-                        <input id="checkbox" type="checkbox"  value=""
-                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-400 me-3 focus:ring-blue-500 focus:ring-2 ">
-                             
-                        {{-- cart image --}}
-                 
-                        <img  class=" h-16 h-16 dark:block   " src="{{ $c_product->product->image }}"
-                            alt="imac image" />
-                        </div>
+                            <input id="checkbox" type="checkbox"  value=""
+                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-400 me-3 focus:ring-blue-500 focus:ring-2 ">
+                                
+                            {{-- cart image --}}
+                    
+                            <img  class=" h-16 h-16 dark:block   " src="{{ $c_product->product->image }}"
+                                alt="imac image" />
+                            </div>
              
-                        <div class=" sm:flex ms-5 sm:w-full ">
-                                          {{-- cart name --}}
-                            <a href="{{ route('products.show', ['product' => $c_product->product->id]) }}" class="hover:underline cursor-pointer  sm:basis-1/2   ">
-                                {{ $c_product->product->name }}</a>
-                   
-                                <div class="flex flex-row items-center sm:basis-full sm:w-full "> 
-                                            {{-- price --}}
-                                    <div class="text-sm items-center font-semibold me-3 text-amber-500 sm:text-xl sm:basis-1/2">
-                                        ₱ {{ $c_product->product->price }}
+                            <div class=" sm:flex ms-5 sm:w-full ">
+                                            {{-- cart name --}}
+                                <a href="{{ route('products.show', ['product' => $c_product->product->id]) }}" class="hover:underline cursor-pointer  sm:basis-1/2   ">
+                                    {{ $c_product->product->name }}</a>
+                    
+                                    <div class="flex flex-row items-center sm:basis-full sm:w-full "> 
+                                                {{-- price --}}
+                                        <div class="text-sm items-center font-semibold me-3 text-amber-500 sm:text-xl sm:basis-1/2">
+                                            ₱ {{ $c_product->product->price }}
+                                        </div>
+                                        {{-- quantity --}}
+                                        <div class="flex items-center    me-3 sm:basis-1/2" x-data="{ count: {{ $c_product->quantity }} }">
+                                            
+                                    
+
+                                            <template x-if="count <= 1">
+                                                <button   x-on:click.prevent="$dispatch('open-modal', 'remove-product-{{$c_product->id}}')"
+                                                    class=" border-gray-300 inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md     hover:bg-gray-200  ">
+                                                    <i class="fa-solid fa-minus"></i>
+                                                </button>
+                                                
+                                            </template>
+                                            
+                                                
+                                            <x-vendor.breeze.modal name="remove-product-{{$c_product->id}}">    
+                                                <form method="post"  class="p-6">
+                                                    @csrf
+                                                    @method('delete')
+                                        
+                                                    <h2 class="text-lg font-medium text-gray-900">
+                                                        {{ __('Remove Product?') }}
+                                                    </h2>
+                                        
+                                                    <p class="mt-1 text-sm text-gray-600">
+                                                        {{ $c_product->product->name }} Will be remove from your cart.
+                                                    </p>
+                                        
+                                                
+                                                    <div class="mt-6 flex justify-end">
+                                                        <x-vendor.breeze.secondary-button x-on:click="$dispatch('close')">
+                                                            {{ __('Cancel') }}
+                                                        </x-vendor.breeze.secondary-button>
+                                        
+                                                        <x-vendor.breeze.danger-button class="ms-3">
+                                                            {{ __('Remove') }}
+                                                        </x-vendor.breeze.danger-button>
+                                                    </div>
+                                                </form>
+                                            </x-vendor.breeze.modal>
+
+                                            <template  x-if="count > 1"> 
+                                            <button type="button" x-on:click="count = count > 0 ? count-1 : count"
+                                                class=" border-gray-300 inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md     hover:bg-gray-200  ">
+                                                <i class="fa-solid fa-minus"></i>
+                                            </button>
+                                        </template>
+                                            <input type="text" x-model="count"
+                                                value=" "  min="1" data-input-counter
+                                                class="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0  "
+                                                required readonly/>
+                                            <button type="button"  x-on:click="count++"
+                                                class="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md     hover:bg-gray-200  ">
+                                                <i class="fa-solid fa-plus"></i>
+                                            </button>
+                                        </div>
+                                        
                                     </div>
-                                    {{-- quantity --}}
-                                    <div class="flex items-center    me-3 sm:basis-1/2">
-                                        <button type="button" id="decrement-button-{{ $c_product->id }}"
-                                            data-input-counter-decrement="counter-input-{{ $c_product->id }}"
-                                            class=" border-gray-300 inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md     hover:bg-gray-200  ">
-                                            <i class="fa-solid fa-minus"></i>
-                                        </button>
-                                        <input type="text" id="counter-input-{{ $c_product->id }}"
-                                            value="{{ $c_product->quantity }}" data-input-counter-min="1" data-input-counter
-                                            class="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0  "
-                                            required />
-                                        <button type="button" id="increment-button-{{ $c_product->id }}"
-                                            data-input-counter-increment="counter-input-{{ $c_product->id }}"
-                                            class="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md     hover:bg-gray-200  ">
-                                            <i class="fa-solid fa-plus"></i>
-                                        </button>
-                                    </div>
+                        
                             
-                                </div>
-                      
-                         
-                    </div>
-                    <div class="   text-end  text-sm">
-                        <form action="">
-                            <input type="submit" value="Remove"
-                                class="text-red-500 text-xs hover:underline cursor-pointer">
-                        </form>
-                    </div>  
+                        </div>
+                        <div >
+                
+                                <button type="submit" x-on:click.prevent="$dispatch('open-modal', 'remove-product-{{$c_product->id}}')"
+                                    class="text-white text-xs hover:underline cursor-pointer bg-red-500 p-3 rounded  ">
+                                    <i class="fa-solid fa-trash"></i>
+                                </button>
+                            </form>
+                        </div>  
 
                     </div>
                 @empty
 
-                    <x-shared.empty />
+                    <x-shared.empty />  
                 @endforelse
             </div>
-            <div class="bg-white mt-5 p-1 rounded rounded-sm sticky   bottom-0 left-0 z-50 w-full h-36 mb-3  p-3">
+            <div class="bg-white mt-5 p-1 rounded rounded-sm sticky   bottom-0 left-0 z-10 w-full h-36 mb-3  p-3">
             
             
               
@@ -93,7 +133,7 @@
                     <a href="checkout"  id=" "
                     class="flex justify-center  text-white bg-amber-500 border-0 py-2 mb-1 sm:mb-0   w-full sm:mb-4 px-4  
                     focus:outline-none hover:bg-amber-600 sm:order-2">
-                    Checkout 23
+                    Checkout  <span x-text="status.length"></span>
                     </a>
              
                         <a href="{{ route('customer.index') }}"
