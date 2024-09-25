8360f0120d1b79865bb63044f3744eac14e32c6b
dnlsrb <144025551+dnlsrb@users.noreply.github.com>
2024-09-25 21:59:53 +0800

    Update seller-index.blade.php

--- resources/views/components/layouts/seller-index.blade.php
+++ resources/views/components/layouts/seller-index.blade.php
@@ -1,12 +1,12 @@
 <x-layouts.app  >
   <div  x-data="{ open: false}"> 
 
-  <span  @click.outside="open = false"     > 
+  <span     > 
   <x-seller.nav/>
   <x-seller.sidebar/>
   </span>
 
-  <div    :class="open ? 'sm:ml-64' : ' sm:ml-20 '"   class="p-4  "    >
+  <div    class="p-4  sm:ml-20  "  >
       {{$slot}}
 
 
