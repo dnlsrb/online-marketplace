1d5c0a0cda25710ec65557079b3a4f5eb0b5642a
arielrecto <arielrecto29@gmail.com>
2024-10-10 23:20:42 +0800

    chore: add new TableBody and TableRow components

---
+++ resources/views/components/shared/table-body.blade.php
@@ -0,0 +1,53 @@
+@props([
+    'columns' => ['name', 'Email', 'Tenement'],
+    'label' => 'sample table',
+    'create_url' => null,
+    'archived_url' => null,
+])
+
+<div class="w-full h-auto flex flex-col gap-2">
+    <div class="w-full flex  items-center justify-between">
+        <h1 class="text-2xl font-bold tex">{{ $label }}</h1>
+        <div class="flex items-center space-x-2">
+            @if ($archived_url)
+                <a href="{{ $archived_url }}" class="btn btn-sm btn-gray-200 text-primary">
+                    <span>
+                        <i class="fi fi-rr-box"></i>
+                    </span>
+                    <span>
+                        Archived
+                    </span>
+                </a>
+            @endif
+            @if ($create_url)
+                <a href="{{ $create_url }}" class="btn btn-sm btn-primary text-accent">
+                    <span>
+                        <i class="fi fi-rr-add"></i>
+                    </span>
+                    <span>
+                        Create
+                    </span>
+                </a>
+            @endif
+        </div>
+
+    </div>
+
+    <div class="overflow-y-auto">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 ">
+            <!-- head -->
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+                <tr>
+                    <th class="px-6 py-3"></th>
+                    @foreach ($columns as $column)
+                        <th class="px-6 py-3">{{ $column }}</th>
+                    @endforeach
+                    <th class="text-center">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {{ $slot }}
+            </tbody>
+        </table>
+    </div>
+</div>
