c5fa3904065c064c7518ba0f0027bbce663e3811
arielrecto <arielrecto29@gmail.com>
2024-10-10 23:22:55 +0800

    chore: add customer controller

---
+++ app/Http/Controllers/Customer/OrderController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Customer;
+
+use App\Enums\OrderStatus;
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $order = Order::create([
+            'order_number' => 'ORDR-' . uniqid(),
+            'product_id' => $request->productId,
+            'user_id' => Auth::user()->id,
+            'quantity' => $request->quantity,
+            'total' => $request->total,
+            'status' => OrderStatus::ORDERED->value
+        ]);
+
+
+        return back()->with(['message_success' => 'Order Success']);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+       
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
