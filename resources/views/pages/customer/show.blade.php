f3edbff1e39080cc7a1c2c913a6c1d2cc5cf2e0c
arielrecto <arielrecto29@gmail.com>
2024-11-02 19:41:38 +0800

    chore(customer)(product) : optional rendering

--- resources/views/pages/customer/show.blade.php
+++ resources/views/pages/customer/show.blade.php
@@ -16,9 +16,9 @@
     <img class="p-1 rounded-t-lg object-fill   w-full    sm:max-w-96      "   src="{{$product->image}}" alt="product image" />
     </div>
   <section class="py-0  sm:px-10 w-full">
- 
+
     <div class="flex justify-between  ">
-      
+
       <h4 class="text-2xl subpixel-antialiased font-semibold">{{$product->name}}   </h4>
 
   <a href="{{route('customer.chat.index', ['product_id' => $product->id])}}" class="  text-black bg-gray-100 shadow
@@ -42,32 +42,40 @@
     <h4 class="text-amber-500 text-3xl font-bold">₱{{$product->price}}</h4>
 
 
-
+    @if (Auth::user()->profile)
     <form action="/customer/cart/add-to-cart/{{$product->id}}" method="POST">
-      @csrf
-      <div class="my-5">
-      <label for="quantity-input" class="block mb-2 text-sm font-medium text-gray-900  ">Quantity:</label>
-      <div class="relative flex items-center max-w-[8rem]">
-          <button type="button" id="decrement-button" data-input-counter-decrement="quantity-input" class="   hover:bg-gray-200 border border-gray-300   p-3 h-11 focus:ring-gray-100   focus:outline-none">
-            <i class="fa-solid fa-minus"></i>
-          </button>
-          <input type="text" name="quantity" data-input-counter-min="1" id="quantity-input" data-input-counter aria-describedby="helper-text-explanation"
-          class="  border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm   block w-full py-2.5   dark:placeholder-gray-400 "
-            value="1" required />
-          <button type="button" id="increment-button" data-input-counter-increment="quantity-input" class="   hover:bg-gray-200 border border-gray-300  p-3 h-11 focus:ring-gray-100   focus:outline-none">
-            <i class="fa-solid fa-plus"></i>
-          </button>
-      </div>
-      </div>
-      <div class="flex my-5">
+        @csrf
+        <div class="my-5">
+        <label for="quantity-input" class="block mb-2 text-sm font-medium text-gray-900  ">Quantity:</label>
+        <div class="relative flex items-center max-w-[8rem]">
+            <button type="button" id="decrement-button" data-input-counter-decrement="quantity-input" class="   hover:bg-gray-200 border border-gray-300   p-3 h-11 focus:ring-gray-100   focus:outline-none">
+              <i class="fa-solid fa-minus"></i>
+            </button>
+            <input type="text" name="quantity" data-input-counter-min="1" id="quantity-input" data-input-counter aria-describedby="helper-text-explanation"
+            class="  border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm   block w-full py-2.5   dark:placeholder-gray-400 "
+              value="1" required />
+            <button type="button" id="increment-button" data-input-counter-increment="quantity-input" class="   hover:bg-gray-200 border border-gray-300  p-3 h-11 focus:ring-gray-100   focus:outline-none">
+              <i class="fa-solid fa-plus"></i>
+            </button>
+        </div>
+        </div>
+        <div class="flex my-5">
 
-        <input type="submit" value="Add to cart" name="submit" class=" px-8 text-amber-900 outline-amber-500 bg-amber-300 outline-2 outline border-0 py-2 px-8 focus:outline-none hover:bg-amber-100 text-sm sm:text-lg me-5">
-        <a href={{route('customer.products.buy-now', ['product' => $product->id])}} class="  text-white bg-amber-500 border-0 py-2 px-8 focus:outline-none hover:bg-amber-700 text-sm sm:text-lg me-5">Buy now</a>
+          <input type="submit" value="Add to cart" name="submit" class=" px-8 text-amber-900 outline-amber-500 bg-amber-300 outline-2 outline border-0 py-2 px-8 focus:outline-none hover:bg-amber-100 text-sm sm:text-lg me-5">
+          <a href={{route('customer.products.buy-now', ['product' => $product->id])}} class="  text-white bg-amber-500 border-0 py-2 px-8 focus:outline-none hover:bg-amber-700 text-sm sm:text-lg me-5">Buy now</a>
 
-      </div>
-    </form>
+        </div>
+      </form>
 
-     
+      @else
+
+      <a href="{{route('profile.edit')}}" class="bg-gray-200 p-2 rounded-lg text-gray-500 hover:bg-gray-500 hover:duration-700 hover:scale-105 hover:text-white my-5">
+        Complete setup your additional profile information
+      </a>
+    @endif
+
+
+
 <div class="sm:flex bg-white p-5 my-4 border">
   <section class="bg-white   flex justify-between w-full">
     <h2 class="   text-lg font-medium   ">{{$product->user->name}}</h2>
@@ -78,10 +86,10 @@
   </section>
   </div>
 </div>
- 
+
 
 {{-- SELLER --}}
- 
+
 
 {{-- PRODUCT REVIEWS --}}
 <div class="sm:flex bg-white p-5 my-4">
@@ -154,10 +162,10 @@
        {{-- ^ add link here  --}}
        ratings="{{rand(1, 5)}}"
        imagesrc="{{$product->image}}"
-       productname="{{ $product->name}}" 
+       productname="{{ $product->name}}"
        productprice="{{ $product->price}}" />
 
-           
+
        @empty
 
        <x-shared.empty/>
