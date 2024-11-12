d4fb0a56af6e575c6531f89330a7ff3160855229
dnlsrb <144025551+dnlsrb@users.noreply.github.com>
2024-11-12 19:11:49 +0800

    fix subcription

--- resources/views/pages/customer/subcription.blade.php
+++ resources/views/pages/customer/subcription.blade.php
@@ -7,7 +7,7 @@
         
         <div class="flex flex-col text-center w-full mb-20">
           <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Choose the right plan for your business</h1>
-          <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical.</p>
+          {{-- <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical.</p> --}}
         </div>
 
 
--- resources/views/pages/profile/seller.blade.php
+++ resources/views/pages/profile/seller.blade.php
@@ -39,7 +39,7 @@
                
                 
                  
-                @if($subscribe->subscription->name)
+                @if(!empty($subscribe->subscription->name))
                 <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg ">
                      
                      
